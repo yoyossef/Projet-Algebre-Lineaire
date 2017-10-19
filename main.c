2ab29145b615589a7d320ebbdda70ff5b594884f
yrostaqi <yossefrostaqi@gmail.com>
2017-10-19 17:32:04 +0200

    Ajouté la fonction main et deleteMatrix

--- main.c
+++ main.c
@@ -9,12 +9,28 @@ typedef struct matrix {
 
 Matrix newMatrix(int nb_rows, int nb_columns){
     Matrix m;
-    E.mat = malloc(nb_rows * nb_columns * sizeof(E));
+    m.mat = malloc(nb_rows * nb_columns * sizeof(E));
     m.nb_rows = nb_rows;
     m.nb_columns = nb_columns;
     return m;
 }
 
 E getElt(Matrix m, int row, int column){
-    return m.mat[row*(m.nb_columns - 1) + m.nb_columns - 1 - m.nb_rows];
+    return m.mat[row*(m.nb_columns - 1) + column - 1 - row];
+}
+
+void setElt(Matrix m, int row, int column, E val){
+    m.mat[row*(m.nb_columns - 1) + column - 1 - row] = val;
+}
+
+void deleteMatrix(Matrix m){
+    free (m.mat);
+}
+
+int main(){
+    Matrix m = newMatrix(5,5);
+    setElt(m, 1,1, 2.5);
+    E elt = getElt(m, 1, 1);
+    printf("%f\n", elt);
+    deleteMatrix(m);
 }
