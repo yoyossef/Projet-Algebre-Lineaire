edece619d72772a49ebb7e0fb25356b4379fa49e
yrostaqi <yossefrostaqi@gmail.com>
2017-11-17 14:43:50 +0100

    Added the Extraction function

--- m_resol.c
+++ m_resol.c
@@ -1,6 +1,37 @@
 #include "m_resol.h"
 #include "tp1.h"
 
+Matrix Extraction(Matrix A, int i, int j){
+    // The extracted Matrix has a dimension of A's - 1
+    Matrix extracted = newMatrix(A.nb_rows - 1, A.nb_columns - 1);
+    int k = 1;
+    int l = 1;
+    int n = 1;
+    int p = 1;
+    for(k = 1; k <= A.nb_rows; k++){
+        // i is the row to ignore
+        if(k == i)
+            continue;
+        // We can't exceed the Matrix's row dimension so we use modulo
+        n = n%A.nb_rows;
+        for(l = 1; l <= A.nb_columns; l++){
+            // j is the column to ignore
+            if(l == j)
+                continue;
+            // We can't exceed the Matrix's column dimension so we use modulo
+            p = p%A.nb_columns;
+            // extracted(n, p) = A(k, l)
+            setElt(extracted, n, p, getElt(A, k, l));
+            // We increment p which is the indice for extracted's column
+            p++;
+        }
+        p++;
+        // We increment n which is the indice for extracted's row
+        n++;
+    }
+    return extracted;
+}
+
 E determinant(Matrix A){
     E det = 0.0;
     int i = 1;
